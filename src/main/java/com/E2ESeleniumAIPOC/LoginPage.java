6b156c89162f633a3274ae8316c2ad71759d98b2
naren21jo <gnarendra.tester@gmail.com>
2025-04-05 20:46:54 +0530

    Add successfully executed generated class: LoginPage

--- src/main/java/com/E2ESeleniumAIPOC/LoginPage.java
+++ src/main/java/com/E2ESeleniumAIPOC/LoginPage.java
@@ -6,25 +6,25 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class LoginPage {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.setProperty("webdriver.edge.driver", "drivers//msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
-        driver.manage().window().maximize();
+		System.setProperty("webdriver.edge.driver", "drivers//msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
 
-        driver.get("https://practicetestautomation.com/practice-test-login/");
-        driver.findElement(By.id("username")).sendKeys("student");
-        driver.findElement(By.id("password")).sendKeys("Password123");
-        driver.findElement(By.id("submit")).click();
+		driver.manage().window().maximize();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
 
-        String actualText = driver.findElement(By.xpath("//h1[@class='post-title']")).getText();
+		driver.findElement(By.id("username")).sendKeys("student");
+		driver.findElement(By.id("password")).sendKeys("Password123");
+		driver.findElement(By.id("submit")).click();
 
-        if (actualText.equals("Logged In Successfully")) {
-            System.out.println("Login successful!");
-        } else {
-            System.out.println("Login failed!");
-        }
+		String homePageText = driver.findElement(By.xpath("//h1[@class='post-title']")).getText();
+		if (homePageText.equals("Logged In Successfully")) {
+			System.out.println("Login successful!");
+		} else {
+			System.out.println("Login failed.");
+		}
 
-        driver.quit();
-    }
+		driver.quit();
+	}
 }
