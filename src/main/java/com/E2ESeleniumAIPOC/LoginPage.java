35fe4213fb521462d54796d2c2f780d04bbe2ebc
naren21jo <gnarendra.tester@gmail.com>
2025-04-13 16:13:15 +0530

    Add successfully executed generated class: LoginPage

--- src/main/java/com/E2ESeleniumAIPOC/LoginPage.java
+++ src/main/java/com/E2ESeleniumAIPOC/LoginPage.java
@@ -3,28 +3,32 @@ package com.E2ESeleniumAIPOC;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class LoginPage {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.setProperty("webdriver.edge.driver", "drivers//msedgedriver.exe");
-		WebDriver driver = new EdgeDriver();
+        System.setProperty("webdriver.edge.driver", "drivers//msedgedriver.exe");
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new EdgeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
 
-		driver.manage().window().maximize();
-		driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
 
-		driver.findElement(By.id("username")).sendKeys("student");
-		driver.findElement(By.id("password")).sendKeys("Password123");
-		driver.findElement(By.id("submit")).click();
+        String actualText = driver.findElement(By.xpath("//h1[@class='post-title']")).getText();
+        String expectedText = "Logged In Successfully";
 
-		String homePageText = driver.findElement(By.xpath("//h1[@class='post-title']")).getText();
-		if (homePageText.equals("Logged In Successfully")) {
-			System.out.println("Login successful!");
-		} else {
-			System.out.println("Login failed.");
-		}
+        if (actualText.equals(expectedText)) {
+            System.out.println("Login successful. Home page verified.");
+        } else {
+            System.out.println("Login failed or home page verification failed.");
+        }
 
-		driver.quit();
-	}
+        driver.quit();
+    }
 }
