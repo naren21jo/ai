c6f1c85c70ee0bbe3cefb8f11770a911566dbedd
naren21jo <gnarendra.tester@gmail.com>
2025-04-15 11:04:39 +0530

    Add successfully executed generated class: LoginPageN

---
+++ src/main/java/com/E2ESeleniumAIPOC/LoginPageN.java
@@ -0,0 +1,31 @@
+package com.E2ESeleniumAIPOC;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LoginPageN {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.edge.driver", "drivers//msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys("student");
+
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("Password123");
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+		WebElement homepageText = driver.findElement(By.xpath("//h1[contains(text(),'Logged In Successfully')]"));
+		homepageText.isDisplayed();
+
+        driver.quit();
+    }
+}
