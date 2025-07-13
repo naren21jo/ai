f6113008358a38b1dae8f1e62ed02d0885f8379e
naren21jo <gnarendra.tester@gmail.com>
2025-07-13 15:47:14 +0530

    Add successfully executed generated class: LoginPage

--- src/main/java/com/E2ESeleniumAIPOC/LoginPage.java
+++ src/main/java/com/E2ESeleniumAIPOC/LoginPage.java
@@ -2,7 +2,6 @@ package com.E2ESeleniumAIPOC;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class LoginPage {
@@ -10,29 +9,20 @@ public class LoginPage {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.edge.driver", "drivers//msedgedriver.exe");
-
         WebDriver driver = new EdgeDriver();
-
         driver.manage().window().maximize();
-
         driver.get("https://practicetestautomation.com/practice-test-login/");
 
-        WebElement usernameField = driver.findElement(By.id("username"));
-        usernameField.sendKeys("student");
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+        
+        String homePageText = driver.findElement(By.xpath("//h1[@class='post-title']")).getText();
 
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("Password123");
-
-        WebElement submitButton = driver.findElement(By.id("submit"));
-        submitButton.click();
-
-        WebElement homePageTextElement = driver.findElement(By.xpath("//h1[contains(text(),'Logged In Successfully')]"));
-        String homePageText = homePageTextElement.getText();
-
-        if (homePageText.contains("Logged In Successfully")) {
-           // System.out.println("Login successful!");
+        if (homePageText.equals("Logged In Successfully")) {
+            System.out.println("Login successful and home page verified.");
         } else {
-           // System.out.println("Login failed!");
+            System.out.println("Login verification failed.");
         }
 
         driver.quit();
